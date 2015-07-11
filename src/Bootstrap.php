99cef7d1139b01bc6b7ec9f2516d8e641e44c6a8
John Martin <john.itvn@gmail.com>
2015-07-11 11:38:29 +0700

    Create Bootstrap.php

---
+++ src/Bootstrap.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace johnitvn\f2ngin;
+
+use yii\base\BootstrapInterface;
+
+/**
+* @author John Martin <john.itvn@gmail.com>
+* @since 1.0
+*/
+class Bootstrap implements BootstrapInterface
+{
+    /**
+     * Bootstrap method to be called during application bootstrap stage.
+     *
+     * @param Application $app the application currently running
+     */
+    public function bootstrap($app)
+    {
+        
+    }
+}
